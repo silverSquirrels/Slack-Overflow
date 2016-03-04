a4031501a7e4a9b380acfce5f314a8f8aa46129e
Eric Broberg <eric.edulis@gmail.com>
2016-03-04 14:37:50 -0800

    (feat) All questions link with all questions shown

--- client/app/posts/posts.js
+++ client/app/posts/posts.js
@@ -14,13 +14,14 @@ angular.module('hackoverflow.posts', [
   $scope.TimeService = TimeService;
   /// add boolean to control whether or not to filter posts
   /// so when user lands on page or clicks 'all forums / homepage' all posts are listed
-  $scope.filterToggler = true;
-  $scope.enableFilter = function() {
-    $scope.filterToggler  = false;
-    console.log('bool', $scope.filterToggler);
+  $scope.forumSelected = false; // a.k.a disableFilter
+  $scope.forumSelect = function() {
+    $scope.forumSelected  = true;
+    console.log('bool', $scope.forumSelected);
   };
-  $scope.disableFilter = function() {
-    $scope.filterToggler = true;
+  $scope.forumDeselect = function() {
+    $scope.forumSelected = false;
+    console.log($scope.forumSelected)
   };
 
   $scope.getPosts = function getPosts(forum) {
