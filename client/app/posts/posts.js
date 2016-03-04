f0293beb35d2473386da66a7d765361e97f74796
Eric Broberg <eric.edulis@gmail.com>
2016-03-04 14:52:15 -0800

    (feat) All questions option / default landing

--- client/app/posts/posts.js
+++ client/app/posts/posts.js
@@ -28,6 +28,7 @@ angular.module('hackoverflow.posts', [
     // TODO: need to pass in forum to Posts.getPosts()
     Posts.getPosts('').then(function (data) {
       $scope.posts = data.data;
+      console.log($scope.posts)
       // this creates an object $scope.numberOfComments that
       // keeps track of each posts number of comments. not
       // ideal, but works. need to refactor how we go
